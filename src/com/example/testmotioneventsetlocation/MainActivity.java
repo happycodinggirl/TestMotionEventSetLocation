1ffe17310e2c5199fa0e41d7da2e4cfa04999449
happycodinggirl <hxlily86@gmail.com>
2014-11-27 16:25:24 +0800

    第一次提交，测试MotionEvent的setLocation方法

---
+++ src/com/example/testmotioneventsetlocation/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.testmotioneventsetlocation;
+
+import java.util.ArrayList;
+
+import android.support.v7.app.ActionBarActivity;
+import android.R.integer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.TextureView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        final ListView listView=(ListView) findViewById(R.id.list);
+        ArrayList<String> datas=new ArrayList<String>();
+        for (int i = 0; i < 80; i++) {
+			datas.add("item"+i);
+		}
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,datas);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "---listview onItemClick",1000).show();
+			}
+		});
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "按钮被点击了",1000).show();
+			}
+		});
+        button.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					float x=event.getX();
+					float y=event.getY();
+					event.setLocation(x, y+25);
+					return listView.dispatchTouchEvent(event);
+					
+					
+				case MotionEvent.ACTION_MOVE:
+					return listView.dispatchTouchEvent(event);
+					
+				case MotionEvent.ACTION_UP:
+					return listView.dispatchTouchEvent(event);
+					
+
+				default:
+					break;
+				}
+				return false;
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
